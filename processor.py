09a5a12350ca240b1d5614b4547a93652f4b7cbc
akshayadhandapani7-ux <akshayadhandapani7@gmail.com>
2026-01-18 19:32:38 +0530

    Update processor.py

--- processor.py
+++ processor.py
@@ -1,1 +1,14 @@
+def process_record(record):
+    """
+    Processes a valid record and returns a cleaned version.
+    """
+
+    processed = {
+        "id": int(record["id"]),
+        "name": record["name"].strip(),
+        "age": int(record["age"]),
+        "salary": int(record["salary"])
+    }
+
+    return processed
 
